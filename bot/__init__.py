79a629cd7b1ced01a54b1676a7f3a6355f4a57f4
XZE-WOLF <76590363+XZE-WOLF@users.noreply.github.com>
2021-07-26 14:39:19 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -35,10 +35,26 @@ load_dotenv('config.env')
 
 Interval = []
 
+<<<<<<< ours
+#version
 USERBOT_VERSION = "1.2 - Alpha"
+lang_code = os.environ.get('lang_code', "en")
+device_model = os.environ.get('device_model', platform.machine())
+system_version = os.environ.get('system_version', platform.platform())
+app_version = "⚙️KenzoMcW v{}".format(USERBOT_VERSION)
+=======
+USERBOT_VERSION = "1.2 - Alpha"
+>>>>>>> theirs
 
+<<<<<<< ours
+# From config
+    Command = os.environ.get("Command", "/")
+    KENZO_WORKERS = int(os.environ.get('KENZO_WORKERS', 8))
+
+=======
 app_version = "⚙️KenzoMcW v{}".format(USERBOT_VERSION)
 
+>>>>>>> theirs
 def getConfig(name: str):
     return os.environ[name]
 
@@ -157,10 +173,16 @@ if DB_URI is not None:
 
 LOGGER.info("Generating USER_SESSION_STRING")
 app = Client(':memory:', api_id=int(TELEGRAM_API), api_hash=TELEGRAM_HASH, bot_token=BOT_TOKEN)
+app2 = Client(USER_SESSION_STRING, api_id=TELEGRAM_API, api_hash=TELEGRAM_HASH, app_version=app_version, device_model=device_model,
+             system_version=system_version, lang_code=lang_code, workers=KENZO_WORKERS)
 
+<<<<<<< ours
+
+=======
 app2 = Client(APP_SESSION, api_id=TELEGRAM_API, api_hash=TELEGRAM_HASH, app_version=app_version, device_model=device_model,
              system_version=system_version, lang_code=lang_code, workers=KENZO_WORKERS, test_mode=TEST_MODE)
 
+>>>>>>> theirs
 #Generate Telegraph Token
 sname = ''.join(random.SystemRandom().choices(string.ascii_letters, k=8))
 LOGGER.info("Generating TELEGRAPH_TOKEN using '" + sname + "' name")
@@ -362,6 +384,12 @@ try:
         TIMEZONE = 'Asia/Jakarta'
 except KeyError:
     TIMEZONE = 'Asia/Jakarta'
+   
+    
+async def edrep(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    await func(**{k: v for k, v in kwargs.items() 
 
 updater = tg.Updater(token=BOT_TOKEN)
 bot = updater.bot
